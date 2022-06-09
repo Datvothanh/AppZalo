3f643b4e47fe950c8ed62c85727397b5860a3f6e
Thành Đạt <93526680+Datvothanh@users.noreply.github.com>
2022-06-09 07:59:12 +0700

    Xóa đăng ký , làm đăng nhập cho zalo

--- app/src/main/java/hcmute/edu/vn/appzalo/LoginActivity.java
+++ app/src/main/java/hcmute/edu/vn/appzalo/LoginActivity.java
@@ -3,9 +3,10 @@ package hcmute.edu.vn.appzalo;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,61 +15,85 @@ import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseException;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+import java.util.concurrent.TimeUnit;
 
 public class LoginActivity extends AppCompatActivity {
-    private EditText emailedit,passedit;
-    private Button btnlogin, btnregis;
-    private FirebaseAuth mAuth;
-
+    private EditText  PhoneNumber;
+    private Button btnregis;
+    private static final String TAG = "LoginActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        TextView btn=findViewById(R.id.textViewSignUp);
-        mAuth=FirebaseAuth.getInstance();
 
-        btnlogin = findViewById(R.id.btnlogin);
+        PhoneNumber=findViewById(R.id.inputPhonenumber);
         btnregis = findViewById(R.id.btnRegister);
 
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(LoginActivity.this,RegisterActivity.class));
-            }
-        });
-
-        btnlogin.setOnClickListener(new View.OnClickListener() {
+        btnregis.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                login();
-            }
-        });
-    }
+                String phoneNumber = PhoneNumber.getText().toString();
+                if(phoneNumber.isEmpty()){
+                    Toast.makeText(LoginActivity.this, "Enter your phone number", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    PhoneAuthProvider.getInstance().verifyPhoneNumber("+84"+phoneNumber, 60, TimeUnit.SECONDS, LoginActivity.this, new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+                        @Override
+                        public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
+                            signInUnser(phoneAuthCredential);
+                        }
 
-    private void login()  { String email, pass;
-        email = emailedit.getText().toString();
-        pass = passedit.getText().toString();
-        if (TextUtils.isEmpty(email)) {
-            Toast.makeText(this, "Please enter email!", Toast.LENGTH_SHORT).show();
-            return;
-        }  if (TextUtils.isEmpty(pass)) {
-            Toast.makeText(this, "Please enter pass!", Toast.LENGTH_SHORT).show();
-            return;
-        }
-        mAuth.signInWithEmailAndPassword(email,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-            @Override
-            public void onComplete(@NonNull Task<AuthResult> task) {
-                if(task.isSuccessful()){
-                    Toast.makeText(getApplicationContext() , "Logged in successfully !", Toast.LENGTH_SHORT).show();
-                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                    startActivity(intent);
-                }else{
-                    Toast.makeText(getApplicationContext() , "Logged unsuccessfully !", Toast.LENGTH_SHORT).show();
+                        @Override
+                        public void onVerificationFailed(@NonNull FirebaseException e) {
+                            Log.d(TAG, "onVerificationFailed: "+ e.getLocalizedMessage());
+                        }
+
+                        @Override
+                        public void onCodeSent(@NonNull String verificationId ,@NonNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+                            super.onCodeSent( verificationId , forceResendingToken);
+
+                            Dialog dialog  = new Dialog(LoginActivity.this);
+                            dialog.setContentView(R.layout.verify_popup);
+                            EditText etVerifyCode = dialog.findViewById(R.id.etVerifyOTP);
+                            Button btnVerifyCode = dialog.findViewById(R.id.btnVerifyOTP);
+                            btnVerifyCode.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View v) {
+                                 String verificationCode = etVerifyCode.getText().toString();
+                                 if(verificationId.isEmpty()) return;
+                                 PhoneAuthCredential credential = PhoneAuthProvider.getCredential(verificationId,verificationCode );
+                                 signInUnser(credential);
+                                }
+                            });
+
+                            dialog.show();
+                        }
+                    });
                 }
             }
         });
     }
+
+    private void signInUnser(PhoneAuthCredential credential) {
+        FirebaseAuth.getInstance().signInWithCredential(credential)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            startActivity(new Intent(LoginActivity.this , MainActivity.class));
+                            finish();
+                        }else {
+                            Log.d(TAG , "onComplete:" + task.getException().getLocalizedMessage());
+                        }
+                    }
+                });
+    }
+
 }
